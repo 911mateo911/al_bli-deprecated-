9f6006c4f97bc21a0aaae1a5253f41223cc14dcc
mateo malaj <malajmateo@gmail.com>
2021-07-14 14:15:14 +0200

    upload

--- pages/api/register-user.js
+++ pages/api/register-user.js
@@ -73,7 +73,6 @@ export default async function handler(req, res) {
             }
         })
     } catch (e) {
-        console.log(e)
         res.send({
             message: 'error',
             errorMsg: 'Ndodhi nje gabim!'
