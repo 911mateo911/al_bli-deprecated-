e4e5e4c2e76c5e54a3c36b705567ce01be1c8ddd
mateo malaj <malajmateo@gmail.com>
2021-07-14 16:32:04 +0200

    try agan

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const webpack = require('webpack')
-const { parsed: myEnv } = require('dotenv').config()
+// const webpack = require('webpack')
+// const { parsed: myEnv } = require('dotenv').config()
 
 module.exports = {
-    webpack(config) {
-        config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-        return config
-    },
+    // webpack(config) {
+    //     config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+    //     return config
+    // },
     eslint: {
         ignoreDuringBuilds: true
     }
