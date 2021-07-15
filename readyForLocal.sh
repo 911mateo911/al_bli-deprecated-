3f10b7900ddc1285c0e84f665c5b4aace50e981e
mateo malaj <malajmateo@gmail.com>
2021-07-15 12:32:20 +0200

    Commiting with name added a shell for local use

---
+++ readyForLocal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ~/Desktop/commitFiles/local/dbConnection.js ~/Desktop/"React Projects"/al-bli-local/al_bli/utils
+echo "u replacua cbConnectioni"
+cp ~/Desktop/commitFiles/local/next.config.js ~/Desktop/"React Projects"/al-bli-local/al_bli
+echo "u replacua next konfigu"
